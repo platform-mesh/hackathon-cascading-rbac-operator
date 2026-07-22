ad08ca5b70d0e19fd8131cde4dd8b18a23d0e841
Cyrill Berg <cyrill.berg@opendefense.cloud>
2026-07-22 15:27:45 +0200

    Add APIExport & ARS objects (#5)
    Signed-off-by: Cyrill Berg <cyrill.berg@bwi.de>

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ import (
 	apisv1alpha1 "github.com/kcp-dev/sdk/apis/apis/v1alpha1"
 	tenancyv1alpha1 "github.com/kcp-dev/sdk/apis/tenancy/v1alpha1"
 
-	cascadev1alpha1 "github.com/platform-mesh/hackathon-cascading-rbac-operator/apis/cascade/v1alpha1"
+	fleetv1alpha1 "github.com/platform-mesh/hackathon-cascading-rbac-operator/apis/fleet/v1alpha1"
 
 	"github.com/kcp-dev/multicluster-provider/apiexport"
 )
@@ -50,7 +50,7 @@ const endpointSliceName = "tenancy.kcp.io"
 func init() {
 	runtime.Must(tenancyv1alpha1.AddToScheme(scheme.Scheme))
 	runtime.Must(apisv1alpha1.AddToScheme(scheme.Scheme))
-	runtime.Must(cascadev1alpha1.AddToScheme(scheme.Scheme))
+	runtime.Must(fleetv1alpha1.AddToScheme(scheme.Scheme))
 }
 
 func main() {
